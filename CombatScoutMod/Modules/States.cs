486c3ee4adf13f237ea72571a584b4609d68815b
GuillermoOM <guille8ameou@gmail.com>
2022-01-05 18:48:09 -0700

    main skill

---
+++ CombatScoutMod/Modules/States.cs
@@ -0,0 +1,23 @@
+﻿using CombatScoutMod.SkillStates;
+using CombatScoutMod.SkillStates.BaseStates;
+using System.Collections.Generic;
+using System;
+
+namespace CombatScoutMod.Modules
+{
+    public static class States
+    {
+        internal static List<Type> entityStates = new List<Type>();
+
+        internal static void RegisterStates()
+        {
+            entityStates.Add(typeof(BaseShootAttack));
+
+            entityStates.Add(typeof(Slash));
+
+            entityStates.Add(typeof(Roll));
+
+            entityStates.Add(typeof(ThrowBomb));
+        }
+    }
+}
